22739f861d5c2fe92d74b6fe241dcd9ca7f78fca
Elang Aditya Rahman <elangadityar@gmail.com>
2026-04-20 23:15:38 +0700

    accepted submission

--- leetcode/0076_min_window_substring/main.cpp
+++ leetcode/0076_min_window_substring/main.cpp
@@ -1,4 +1,5 @@
-#include <algorithm>
+#include <climits>
+#include <iostream>
 #include <map>
 #include <string>
 
@@ -8,6 +9,8 @@ class Solution {
 public:
   string minWindow(string s, string t) {
     map<char, int> m;
+    if (s.length() < t.length())
+      return "";
 
     // count all occurence of char in target
     for (auto c : t) {
@@ -16,40 +19,50 @@ public:
     int len = s.length();
     int fulfilled = t.length();
 
-    int l, r = 0; // pointers to string position
-    int L, R = 0; // current minimum window
-    while (l != len && r != len) {
-      if (fulfilled == 0) {
-        if ((r - l) == t.length()) {
-          return s.substr(l, r);
-        }
-
-        if ((r - l) < (R - L)) {
-          R = r;
-          L = l;
-        }
-
-        auto lit = m.find(s[l]);
-        lit->second++;
-        if (lit->second == 1) {
-          fulfilled = t.length();
-          l = r;
-          continue;
-        }
-
-        l++;
-        continue;
-      }
-
-      auto rit = m.find(s[r]);
-      if (rit != m.end()) { // found
-        rit->second--;
-        if (rit->second >= 0) {
+    int l = 0, r = 0;       // pointers to string position
+    int L = 0, R = INT_MAX; // current minimum window
+    while (r != len) {
+      auto it = m.find(s[r]);
+      if (it != m.end()) {
+        it->second--;
+        if (it->second >= 0) {
           fulfilled--;
         }
       }
+
+      while (fulfilled == 0) {
+        if ((r + 1 - l) == t.length()) {
+          return s.substr(l, r + 1 - l);
+        }
+
+        auto it2 = m.find(s[l]);
+        if (it2 != m.end()) {
+          it2->second++;
+          if (it2->second > 0) {
+            fulfilled++;
+
+            if ((r - l) < (R - L)) {
+              R = r;
+              L = l;
+            }
+          }
+        }
+
+        l++;
+      }
       r++;
     }
-    return s.substr(L, R);
+
+    if (R == INT_MAX) {
+      return "";
+    }
+    return s.substr(L, R - L + 1);
   }
 };
+
+int main() {
+  Solution sol;
+  cout << sol.minWindow("ADOBECODEBANC", "ABC") << endl;
+  cout << sol.minWindow("ABC", "AC") << endl;
+  cout << sol.minWindow("A", "C") << endl;
+}
